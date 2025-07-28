8a1a8a072beb7f9c7e85e5466e12de821c18dc25
Gnu <gwoprk98@gmail.com>
2025-07-28 14:37:08 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -8,21 +8,21 @@ int main() {
     int n;
     cin >> n;
 
-    vector<int> dp(n+1, 0);
+    vector<int> dp(n+1);
 
+    dp[1] = 0;
 
     for (int i = 2; i <= n; i++) {
         dp[i] = dp[i-1] + 1;
 
         if (i % 2 == 0) {
-            dp[i] = min(dp[i], dp[i / 2] + 1);
+            dp[i] = min(dp[i], dp[i/2] + 1);
         }
+
         if (i % 3 == 0) {
-            dp[i] = min(dp[i], dp[i / 3] + 1);
+            dp[i] = min(dp[i], dp[i/3] + 1);
         }
     }
 
-    cout << dp[n] << "\n";
-
-    return 0;
+    cout << dp[n];
 }
